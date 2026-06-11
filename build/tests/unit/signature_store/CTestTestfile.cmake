51a233ace43101a21e55ca287d3e0d2a43889e58
Sergio Randriamihoatra <167822166+sergiorandria@users.noreply.github.com>
2026-06-11 21:28:57 +0300

    Delete build directory

--- build/tests/unit/signature_store/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sergio/project/vHSM/tests/unit/signature_store
-# Build directory: /home/sergio/project/vHSM/build/tests/unit/signature_store
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
