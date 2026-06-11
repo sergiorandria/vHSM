5cbdd98e146ef2e52a8e0119472e794400a64df5
sergiorandria <sergiorandriamihoatra@gmail.com>
2026-06-11 20:56:50 +0300

    Fixed key_fingerprint.cpp from_SPKI implementation, by adding proper error handling

---
+++ build/src/keystore/CMakeFiles/vhsm_keystore.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sergio/project/vHSM/src/keystore/attribute_store.cpp" "src/keystore/CMakeFiles/vhsm_keystore.dir/attribute_store.cpp.o" "gcc" "src/keystore/CMakeFiles/vhsm_keystore.dir/attribute_store.cpp.o.d"
+  "/home/sergio/project/vHSM/src/keystore/hsm_object.cpp" "src/keystore/CMakeFiles/vhsm_keystore.dir/hsm_object.cpp.o" "gcc" "src/keystore/CMakeFiles/vhsm_keystore.dir/hsm_object.cpp.o.d"
+  "/home/sergio/project/vHSM/src/keystore/object_store.cpp" "src/keystore/CMakeFiles/vhsm_keystore.dir/object_store.cpp.o" "gcc" "src/keystore/CMakeFiles/vhsm_keystore.dir/object_store.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
