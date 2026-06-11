0c6060b29fd5bcccff4e098ab679e77cb03cb49d
sergiorandria <sergiorandriamihoatra@gmail.com>
2026-06-11 15:55:10 +0300

    Removed build folder

--- build/src/keystore/CMakeFiles/vhsm_keystore.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libvhsm_keystore.a"
-)
--- build/src/keystore/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sergio/project/vHSM/src/keystore
-# Build directory: /home/sergio/project/vHSM/build/src/keystore
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
